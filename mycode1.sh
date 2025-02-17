fc152b05b6512979b798558c5b544b96cafb86a7
Maxime CHAMIS <maxime.chamis@gmail.com>
2025-02-17 10:45:46 +0100

    Added mycode1.sh script

--- mycode1.sh
+++ mycode1.sh
@@ -1,5 +1,5 @@
 echo '#!/bin/bash' > mycode1.sh
-echo 'echo "🚀 Hello, Jenkins from Burger1! 🍔"' >> mycode1.sh
+echo 'echo "🚀 Hellooooooooooaoaaaaaaaaaadddddo, Jenkins from Burger1! 🍔"' >> mycode1.sh
 echo 'echo "📅 Current Date: $(date)"' >> mycode1.sh
 echo 'echo "💻 Running on: $(hostname)"' >> mycode1.sh
 echo 'echo "📂 Current Directory: $(pwd)"' >> mycode1.sh
