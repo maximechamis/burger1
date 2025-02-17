c907edc8aa89613603dab414918e736ae2890508
Maxime CHAMIS <maxime.chamis@gmail.com>
2025-02-17 10:38:15 +0100

    Added mycode1.sh script

---
+++ mycode1.sh
@@ -0,0 +1,7 @@
+echo '#!/bin/bash' > mycode1.sh
+echo 'echo "🚀 Hello, Jenkins from Burger1! 🍔"' >> mycode1.sh
+echo 'echo "📅 Current Date: $(date)"' >> mycode1.sh
+echo 'echo "💻 Running on: $(hostname)"' >> mycode1.sh
+echo 'echo "📂 Current Directory: $(pwd)"' >> mycode1.sh
+echo 'echo "🔍 Listing Files:"' >> mycode1.sh
+echo 'ls -la' >> mycode1.sh
